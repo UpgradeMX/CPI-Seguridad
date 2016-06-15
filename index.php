f9a1fd947aca01764e1585edb0cf46cd324398c8
georgedemian <jorgejesusdamiangutierrez@iMac-de-Jorge.local>
2016-06-15 16:15:36 -0600

    Index

--- index.php
+++ index.php
@@ -13,16 +13,43 @@
     <?php include_once("phpAssets/analytics.php"); ?>
         <div id="big-container"> 
         	 <?php include_once("phpAssets/header.php"); ?>
-             
-<<<<<<< HEAD
-           <p>prueba nat-master</p>
-=======
-<<<<<<< HEAD
-           <p>Prueba de master</p>
-=======
-           <p>prueba nat</p>
->>>>>>> origin/master
->>>>>>> master
+				<main id="index">
+                	<div id="welcome">
+                    <p>Contamos con la experiencia de <br>
+						<span class="jumbo">20 años</span> <br>
+						prestando nuestros servicios de seguridad para la industria, el <br>
+                        comercio, el turismo, la construcción y residencial</p>
+                    </div>
+                    
+                    <div class="index-section">
+                   	<div id="large-header" class="large-header col-50">
+                      <canvas id="demo-canvas"></canvas>
+                        <h1 class="sect-title">CONFIANZA</h1>
+                    </div><div id="ciudades-index" class="col-50">
+                    	<h1 class="sect-title">nuestras ciudades</h1>
+                        <a href="">
+                        <div class="lista-ciudades">
+                       		<p>Chihuahua • Hermosillo • Tijuana • Cancún • Tulum • Mexicali • San José del Cabo • Torreón • Saltillo • Gómez Palacio • Durango • El Salto • Santiago Papasquiaro • Guadalupe Victoria • Rodeo • Cuencamé • Vicente Guerrero • Canatlán • Monterrey • San Pedro Garza García • Reynosa • Matamoros • Río Bravo • San Fernando • Nuevo Laredo • Polanco • Santa Fe • Xochimilco • Ecatepec • Pachuca • Tizayuca • Querétaro de Arteaga • Puebla • Puerto Plata (República Dominicana)</p> 
+                        </div>
+                        </a>
+                    </div>
+                    </div>
+                    
+                    <div id="third" class="index-section">
+                    
+                    </div>
+                    
+                    <div id="last" class="index-section">
+                    <div id="contacto-index" class="col-50">
+                    <img src="_images/galeria/contacto-index-bg.jpg" alt="">
+                    <span>Contacto 01 800 00 (34337)</span>
+                    </div><div id="index-clientes" class="col-50">
+                   	<h1 class="sect-title">algunos de nuestros clientes</h1> 
+                    </div>
+                    
+                    </div>
+                
+                </main>
         	<div id="footer_cut"></div>
         </div><!--END #big-container -->
         
